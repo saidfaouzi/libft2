72832ac09705c944a7d8b2d27ef940210059e9e5
Said Faouzi <sfaouzi@c2r5p10.1337.ma>
2024-11-07 13:57:52 +0100

    version 08

--- ft_tolower.c
+++ ft_tolower.c
@@ -13,6 +13,6 @@
 int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
-		return (c + 32);
-	return (0);
+		c += 32;
+	return (c);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -13,6 +13,6 @@
 int	ft_toupper(int c)
 {
 	if (c >= 97 && c <= 122)
-		return (c - 32);
-	return (0);
+		c += 32;
+	return (c);
 }
